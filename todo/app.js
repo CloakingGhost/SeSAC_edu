13f0ee93a234e308def43f1f23967c6609f5e517
JaeHyeon_Lee <project0858@gmail.com>
2024-11-03 17:00:09 +0900

    1. 버튼에 이벤트 추가 => 상위 요소에 이벤트 추가로 변경 2. region으로 코드 부분 2-1 함수마다 doc 작성
    타인과 공유 할 때 별도의 설명없이 재사용 가능한 코드를 만들기 위함
    미래에 코드를 다시 보았을 때 분석하는 시간을 줄이기 위함
    유지보수를 용이하게 만들기 위함

--- todo/app.js
+++ todo/app.js
@@ -1,98 +1,139 @@
-// API 베이스 URL
-const URL = 'http://localhost:3000/todos';
+//#region API URL
+
+const TODOS_URL = 'http://localhost:3000/todos';
 const INDEX_URL = 'http://localhost:3000/index';
+//#endregion
+
+//#region 초기 변수
+const todoList = document.querySelector('#todo-list');
+const addTodoBtn = document.querySelector('#add-todo');
+const contentInput = document.querySelector('#todo-input');
+let isSubmitting = false; // 중복 전송 방지
+//#endregion
+//#region 초기 변수에 이벤트 추가
 
 // DOMContentLoaded : HTML 문서 로딩이 끝나면 실행되는 이벤트
 // 페이지가 로드되면 Todo 목록 초기화 함수를 실행한다.
 document.addEventListener('DOMContentLoaded', initTodos);
-const todoList = document.querySelector('#todo-list');
-const addTodoBtn = document.querySelector('#add-todo');
-const content = document.querySelector('#todo-input');
 
+// 최초 화면 구성
+todoList.addEventListener('click', async (e) => {
+  const target = e.target;
 
+  // 완료버튼: 완료 상태 토글링 => DB(비동기=> 상태 변경 반영), UI(최소선)
+  if (target.classList.contains('btn-info')) {
+    const li = target.closest('li');
+    const id = li.querySelector('input[name=id]').value;
+    const content = li.querySelector('.content').textContent.trim();
 
-let isSubmitting = false; // 중복 전송 방지
-/* Create */
+    const todoDiv = li.querySelector('.todo');
+    const isCompleted = todoDiv.dataset.completed === 'true';
+    const obj = { id, content, completed: !isCompleted };
+
+    const result = await statusToggle(obj);
+    if (result) {
+      statusToggleDisplay(target);
+      todoDiv.dataset.completed = !isCompleted;
+    }
+  }
+  // 삭제버튼: 목록 삭제
+  else if (target.classList.contains('btn-danger')) {
+    const li = target.closest('li');
+    const id = li.querySelector('input[name=id]').value;
+
+    const todoDiv = li.querySelector('.todo');
+    const isCompleted = todoDiv.dataset.completed === 'true';
+    const obj = { id, completed: isCompleted };
+    // 확인 후 삭제 진행
+    if (isDelete(isCompleted)) {
+      const result = await removeTodo(obj);
+      if (result) {
+        removeTodoDisplay(target);
+      }
+    }
+  }
+});
+
+// 목록 추가: 추가버튼 클릭
 addTodoBtn.addEventListener('click', async (e) => {
   // 중복 전송 방지
   e.preventDefault();
   if (isSubmitting) return;
   isSubmitting = true;
-  // 추가 버튼 누른다
-  // 인풋태그 값 가져온다
-  // axios로 DB에 보낸다
-  // 등록된거 확인 되면
-  // 화면에 뿌린다
+
   const error = document.querySelector('#input-error');
   const validationObj = validateValue(content.value);
   content.classList.remove('valid');
+  // 유효성 검사
   if (validationObj.result) {
-    const response = await axios.post(URL, await createTodo(content.value));
+    const response = await axios.post(
+      TODOS_URL,
+      await createTodo(content.value)
+    );
     const data = response.data;
 
     const li = createLiTag(data);
     todoList.append(li);
     error.textContent = '';
   } else {
-    content.classList.add('valid');
+    contentInput.classList.add('valid');
     error.textContent = validationObj.message;
   }
-  content.value = '';
+  contentInput.value = '';
   isSubmitting = false; // 완료 후 플래그 초기화
 });
-content.addEventListener('keydown', (e) => {
+
+// 목록 추가: 엔터
+contentInput.addEventListener('keydown', (e) => {
   if (isEnterKey(e) && !isSubmitting) {
     e.preventDefault();
     addTodoBtn.click();
   }
 });
-function isEnterKey(e) {
-  return e.keyCode === 13 || e.key === 'Enter' || e.code === 'Enter';
-}
-// todo li 생성
+
+//#endregion
+
+//#region 함수 CRUD
+
+/**
+ * @description Todo 목록 추가용 li 태그 생성
+ * @param {object} todo
+ * @returns {HTMLLIElement} return HTMLLIElement
+ */
 function createLiTag(todo) {
   const li = document.createElement('li');
   li.insertAdjacentHTML('beforeend', createTodoHTML(todo));
-  // li 안에 값(요소들) 넣고 버튼 찾아서 이벤트 추가
-  const completedBtn = li.querySelector('.btn-info');
-  const deleteBtn = li.querySelector('.btn-danger');
-  // 클로저: todo가 남아있음, e를 통해 데이터를 찾는 작업 안해도 됨
-  completedBtn.addEventListener('click', async (e) => {
-    todo = await statusToggle(todo);
-    statusToggleDisplay(e);
-  });
-  // 클로저: todo가 남아있음
-  deleteBtn.addEventListener('click', async (e) => {
-    const result = await removeTodo(todo);
-    
-    if (result) {
-      removeTodoDisplay(e);
-    }
-  });
   // 페이지 로딩 때 취소선
   if (todo.completed) {
     li.querySelector('div.content').classList.add('del');
   }
   return li;
 }
-// DB에 추가
-async function createTodo(content) {
+/**
+ * @description 목록 추가를 위한 데이터 모음 생성
+ *
+ * @param {string} content
+ * @returns {object} POST를 위한 데이터
+ */
+async function makeNewTodo(content) {
   const response = await axios(INDEX_URL);
   const index = response.data['index'];
   const updateIndex = await axios.put(INDEX_URL, { index: index + 1 });
   return {
     id: `${index}`,
-    content: content,
+    content,
     completed: false,
   };
 }
+/**
+ * @description object의 데이터 매핑 => string
+ * @param {object} todo
+ * @returns {string} 텍스트 형태의 HTML 코드
+ */
 function createTodoHTML(todo) {
-  /* input "${todo.completed}" 삭제 해야함 */
   return `
   <div class="todo" data-completed="${todo.completed}">
     <input type="hidden" name="id" value="${todo.id}">
-    
-    <input type="hidden" name="completed" value="${todo.completed}">
     <div class="content">
       <b>${todo.content}</b>
     </div>
@@ -106,89 +147,68 @@ function createTodoHTML(todo) {
   `;
 }
 /* Update */
-// 완료 버튼 누르면
-// 해당 content에 class 추가  .del {text-decoration: line-through}
-// <input type="hidden" name="completed" value="${todo.completed}">
-// fetch로 DB 바꿔
 
-// completed 바꾸기
+/**
+ * @description (async) todo 상태 변경
+ * @param {object} todo
+ * @returns {object | null} 변경된 todo || null
+ */
 async function statusToggle(todo) {
   try {
-    todo.completed = !todo.completed;
-    const response = await axios.put(`${URL}/${todo.id}`, todo);
+    const response = await axios.put(`${TODOS_URL}/${todo.id}`, todo);
     return response.data;
   } catch (error) {
     document.querySelector('#input-error').textContent =
-      '오류가 발생했습니다. 다시 시도해주세요.';
+      '수정 중 오류가 발생했습니다. 다시 시도해주세요.';
     console.log(error);
     return null;
   }
 }
-function statusToggleDisplay(e) {
-  const todo = e.target.closest('div.todo');
-  const content = todo.querySelector('div.content');
+/**
+ * @description (UI) todo 상태 변경, 해당 목록에 최소선 토글
+ * @param {EventTarget} target 
+ */
+// completed 상태 바꾸기(UI)
+function statusToggleDisplay(target) {
+  const todoDiv = target.closest('div.todo');
+  const content = todoDiv.querySelector('div.content');
   content.classList.toggle('del');
 }
-
-/* Delete */
-// 삭제
-// 삭제 전에 comfirm 입력 받아서
-// 축하드립니다 목표를 달성하셨습니다. 해당 목표를 삭제하시겠습니까?
-// 아직 목표를 달성하지 못했습니다. 목표를 삭제하시겠습니까?
-
-// ture 일 경우
-// id 값 가지고
-//     id 값????? DB에 index 값 하나 만들어야 될듯?
-// 해당 값 DB에서 제거
-// 제거 후 화면에서도 제거
-// 아님 일부만 지울까? <- 이게 UX 더 좋긴 함
+/**
+ * @description (async) 목록 삭제
+ * @param {object} todo 
+ * @returns {object | null} 삭제된 데이터 || null
+ */
 async function removeTodo(todo) {
   try {
-    if (checkDel(todo.completed)) {
-      const response = await axios.delete(`${URL}/${todo.id}`);
-      return true;
-    } else {
-      return false
-    }
+    const response = await axios.delete(`${TODOS_URL}/${todo.id}`);
+    return response.data;
   } catch (error) {
     document.querySelector('#input-error').textContent =
-      '오류가 발생했습니다. 다시 시도해주세요.';
+      '삭제 중 오류가 발생했습니다. 다시 시도해주세요.';
     console.error(error);
-    return false;
+    return null;
   }
-  // DB에서 내용 지우기
-  // 화면에서 내용 지우기
-  // todo.closest('li').remove();
 }
-function removeTodoDisplay(e) {
-  const todoLi = e.target.closest('li');
+/**
+ * @description 이벤트에 해당하는 목록을 화면에서 삭제
+ * @param {EventTarget} target 
+ */
+function removeTodoDisplay(target) {
+  const todoLi = target.closest('li');
   todoLi.remove();
 }
-/* Init */
-async function initTodos() {
-  const response = await axios.get(URL);
-  const data = response.data;
-  data.forEach((todo) => {
-    const li = createLiTag(todo);
-    todoList.append(li);
-  });
-}
+//#endregion
 
-/* Validation */
+//#region 함수 유효성 검사
 
-// 유효성 검사 뭐가 있을까
-// 빈값일 경우 => 입력값이 없습니다.
-// 최소 2글자 이상, trim 사용 => 2글자 이상 입력해주세요
-
-/* Delete 전용 */
-function checkDel(flag) {
-  const message = flag
-    ? '축하드립니다🎉 목표를 달성하셨습니다. 삭제하시겠습니까?'
-    : '아직 목표를 달성하지 못했습니다. 삭제하시겠습니까?';
-  return confirm(message);
-}
-
-/* Create 전용 */
+/**
+ * @description 목록 추가시 입력값에 대한 유효성 검사
+ * 1. 입력값 없음
+ * 2. 최소 글자수 
+ * @param {string} v 
+ * @returns {object} result: boolean, message: string
+ */
 function validateValue(v) {
   const minimumCharacters = 2;
   v = v.trim();
@@ -204,3 +224,41 @@ function validateValue(v) {
   }
   return { result: result, message: message };
 }
+//#endregion
+
+//#region 유틸(그 외 기능)
+
+/**
+ * @description (async) 화면 랜더링 후 실행
+ */
+async function initTodos() {
+  const response = await axios.get(TODOS_URL);
+  const data = response.data;
+  data.forEach((todo) => {
+    const li = createLiTag(todo);
+    todoList.append(li);
+  });
+}
+
+/**
+ * @description 클라이언트의 삭제여부 확인
+ * @param {boolean} flag 목록 완료 여부
+ * @returns {boolean} 
+ */
+function isDelete(flag) {
+  const message = flag
+    ? '축하드립니다🎉 목표를 달성하셨습니다. 삭제하시겠습니까?'
+    : '아직 목표를 달성하지 못했습니다. 삭제하시겠습니까?';
+  return confirm(message);
+}
+
+/**
+ * @description Enter key 확인: input 태그의 입력키
+ * @param {Event} e 
+ * @returns {boolean}
+ */
+function isEnterKey(e) {
+  return e.keyCode === 13 || e.key === 'Enter' || e.code === 'Enter';
+}
+
+//#endregion
