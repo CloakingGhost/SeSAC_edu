4b2d172902ea0a107cfadd8c6b63edc401c53609
JaeHyeon_Lee <project0858@gmail.com>
2024-11-06 22:31:10 +0900

    BOJ 7785 set -> list 변환 list.sort || sorted 역정렬

---
+++ coding_test_python/7785.py
@@ -0,0 +1,13 @@
+n = int(input())
+my_set = set()
+for _ in range(n):
+  name, status = input().split()
+  if status == 'enter':
+    my_set.add(name)
+  else :
+    my_set.remove(name)
+
+answer = list(my_set)
+answer.sort(reverse=True)
+for n in answer:
+  print(n)
